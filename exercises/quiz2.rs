43ec975780dcdca7476ae8bd60a9ae68be58fdd9
WALLI001 <15600915554@163.com>
2023-08-10 22:29:00 +0800

    080910

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,9 +32,11 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+
+
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output:  Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
         }
@@ -45,13 +47,13 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
-    use super::Command;
-
+     use crate::my_module::transformer;
+     use std::process::Command;
+     
     #[test]
     fn it_works() {
         let output = transformer(vec![
-            ("hello".into(), Command::Uppercase),
+            ("HELLO".into(), Command::Uppercase),
             (" all roads lead to rome! ".into(), Command::Trim),
             ("foo".into(), Command::Append(1)),
             ("bar".into(), Command::Append(5)),
